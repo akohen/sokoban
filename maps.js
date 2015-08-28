35ef3e0b9b6e88f5ab7e7f21d57c8023b64d2b92
Alexandre KOHEN <alexandre@kohen.fr>
2015-08-28 09:17:08 +0200

    Version 2

---
+++ maps.js
@@ -0,0 +1,50 @@
+var maps = new Array();
+
+map = new Array();
+map.push("XXXXXXXXXXXXXXXXXXX");
+map.push("X                 X");
+map.push("X @ B          o  X");
+map.push("X                 X");
+map.push("X                 X");
+map.push("X                 X");
+map.push("XXXXXXXXXXXXXXXXXXX");
+maps.push(map);
+
+map = new Array();
+map.push("XXXXXXXXXXXXXXXXXXX");
+map.push("X                 X");
+map.push("X @ B          o  X");
+map.push("X            B o  X");
+map.push("X                 X");
+map.push("X                 X");
+map.push("XXXXXXXXXXXXXXXXXXX");
+maps.push(map);
+
+map = new Array();
+map.push("XXXXXXXXXXX");
+map.push("X oooXooo X");
+map.push("X   B@B   X");
+map.push("X BBXXXBB X");
+map.push("X   X X   X");
+map.push("XXXXX XXXXX");
+maps.push(map);
+
+map = new Array();
+map.push("XXXXXXXXXXXXXXXXXXX");
+map.push("X         X    X  X");
+map.push("X @ B     X   Xo  X");
+map.push("X             XX  X");
+map.push("X         X       X");
+map.push("X         X       X");
+map.push("XXXXXXXXXXXXXXXXXXX");
+maps.push(map);
+
+map = new Array();
+map.push("XXXXXXXXXXXXXXXXXXX");
+map.push("X         X    X  X");
+map.push("X @ B          o  X");
+map.push("X             XX  X");
+map.push("X         X       X");
+map.push("X         X       X");
+map.push("XXXXXXXXXXXXXXXXXXX");
+maps.push(map);
